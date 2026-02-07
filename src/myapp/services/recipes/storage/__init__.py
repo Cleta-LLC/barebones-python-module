82b4f45759e5fdc0feb9c0d44b1e09c0e078eec1
yogac940 <yogaC940@checho.net>
2026-02-07 17:27:46 -0500

    Revert "Add recipes service for baking recipe management"
    This reverts commit a414ee869575ae6cab5f1f1fe737c7740df81d43.

--- src/myapp/services/recipes/storage/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Storage adapters for the recipes service."""
-
-from myapp.services.recipes.storage.json_adapter import RecipesJsonStore
-from myapp.services.recipes.storage.sqlite_adapter import RecipesSqliteStore
-
-__all__ = ["RecipesJsonStore", "RecipesSqliteStore"]
